30d55e688717c982581bbf79fc372e54671a0884
Jay Emerald V. Chaves <126301798+chavesjayemerald@users.noreply.github.com>
2024-05-30 01:31:04 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require("express");
+const http = require('http');
+const socketIo = require('socket.io');
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-require("dotenv").config()
+require("dotenv").config();
 
 const cors = require("cors");
 const session = require('express-session');
-const { collection } = require("./models/userModel");
 const app = express();
-const PORT = process.env.PORT | 5000
+const server = http.createServer(app);
+const io = socketIo(server);
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected."))
@@ -20,10 +23,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use(session({
-        secret: "ballsballs",
-        saveUninitialized: true,
-        resave: false,
-    })
+    secret: "secretKey",
+    saveUninitialized: true,
+    resave: false,
+})
 );
 
 app.use((req, res, next) => {
@@ -38,7 +41,7 @@ app.get("/", (req, res) => {
 
 app.get("/signup", (req, res) => {
     res.render("signup");
-})
+});
 
 app.use('/uploads', express.static('uploads'));
 
@@ -48,4 +51,22 @@ app.use(express.static("public"));
 
 app.use("", require("./routes/routes"));
 
-app.listen(PORT, () => console.log(`Listening at ${PORT}`));
+app.get("/chat", (req, res) => {
+    res.render("chat");
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        // Echo the message back down the socket to all connected clients
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(PORT, () => console.log(`Listening at ${PORT}`));
